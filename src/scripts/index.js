4b9e249b0d823e234657bceedf8f037e4a7b5e01
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2021-12-27 10:07:40 +0700

    update components scss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,11 @@
-import '../../node_modules/jquery/dist/jquery.min';
-import '../styles/bootstrap/css/bootstrap.css';
-import '../styles/bootstrap/js/bootstrap';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 import '../styles/css/main.css';
+
+const nav = document.querySelector('.navbar-fixed-top');
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 20) {
+    nav.classList.add('sticky', 'shadow-sm');
+  } else {
+    nav.classList.remove('sticky', 'shadow-sm');
+  }
+});
